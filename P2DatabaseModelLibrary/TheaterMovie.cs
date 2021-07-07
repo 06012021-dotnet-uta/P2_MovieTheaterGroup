55027d66b4bb69ddea33d7b6ba8831150541794b
GregoryAgnew <gregory.agnew@revature.net>
2021-07-07 13:33:06 -0500

    Added database scaffold

---
+++ P2DatabaseModelLibrary/TheaterMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace P2DatabaseModelLibrary
+{
+    public partial class TheaterMovie
+    {
+        public string MovieId { get; set; }
+        public int TheaterId { get; set; }
+
+        public virtual Movie Movie { get; set; }
+        public virtual Theater Theater { get; set; }
+    }
+}
