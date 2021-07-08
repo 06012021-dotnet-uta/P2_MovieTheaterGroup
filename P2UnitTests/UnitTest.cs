43d6deb711f379af1725839af6ff1d9e77f0eb49
GregoryAgnew <gregory.agnew@revature.net>
2021-07-08 08:17:26 -0500

    added some methods for commit and rating

---
+++ P2UnitTests/UnitTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using QueryModels;
+using Microsoft.EntityFrameworkCore;
+using P2DatabaseModelLibrary;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2UnitTests
+{
+    public class UnitTest
+    {
+        DbContextOptions<P2Context> options = new DbContextOptions<P2Context>()
+            
+        [Fact]
+        public void TestCreateComment()
+        {
+
+        }
+    }
+}
---
+++ QueryModels/Comment.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using P2DatabaseModelLibrary;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace QueryModels
+{
+    /// <summary>
+    /// Contains methods for interacting with the Comments table in the database.
+    /// </summary>
+    public class Comment
+    {
+        private readonly P2Context _context;
+
+        /// <summary>
+        /// Initializes an instance of the database models.
+        /// </summary>
+        /// <param name="context"></param>
+        public Comment(P2Context context)
+        {
+            _context = context;
+        }
+        DateTime current = DateTime.Now;
+
+        /// <summary>
+        /// Creates a comment in the database.
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="movieid"></param>
+        /// <param name="content"></param>
+        public async void CreateCommentAsync(int userid, string movieid, string content)
+        {
+            var comment = _context.Comments.Where(x => (x.UserId == userid) && (x.MovieId == movieid)).FirstOrDefault();
+            comment.Content = content;
+            comment.UserId = userid;
+            comment.DateMade = current;
+            comment.MovieId = movieid;
+            _context.Add(comment);
+            await _context.SaveChangesAsync();
+        }
+        /// <summary>
+        /// Deletes a comment from the database.
+        /// </summary>
+        /// <param name="commentid"></param>
+        public async void DeleteCommentAsync(int commentid)
+        {
+            var comment = _context.Comments.Where(x => x.CommentId == commentid).FirstOrDefault();
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+        }
+        /// <summary>
+        /// Updates a comment in the database.
+        /// </summary>
+        /// <param name="commentid"></param>
+        /// <param name="content"></param>
+        public async void UpdateCommentAsync(int commentid, string content)
+        {
+            var comment = _context.Comments.Where(x => x.CommentId == commentid).FirstOrDefault();
+            comment.Content = content;
+            comment.DateMade = current;
+            _context.Update(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Gets all comments for one movie from the database.
+        /// </summary>
+        /// <param name="movieid"></param>
+        /// <returns>Returns a list of strings.</returns>
+        public List<string> ReadCommentsForOneMovie(string movieid)
+        {
+            var comments = _context.Comments.Where(x => x.MovieId == movieid).Select(x=>x.Content).ToList();
+            return comments;
+        }
+
+        /// <summary>
+        /// Gets all comments for one user from the database.
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <returns>Returns a list of strings.</returns>
+        public List<string> ReadCommentsForOneUser(int userid)
+        {
+            var comments = _context.Comments.Where(x => x.UserId == userid).Select(x => x.Content).ToList();
+            return comments;
+        }
+    }
+}
