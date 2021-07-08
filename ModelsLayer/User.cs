61d664c295bb5e69d114d94defaf46c9799a384a
SekouDosso <sekou.dosso@revature.net>
2021-07-08 17:44:50 -0400

    resolving conflict

---
+++ ModelsLayer/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace ModelsLayer
+{
+    public  class User
+    {
+        public User()
+        {
+            Comments = new HashSet<Comment>();
+            Ratings = new HashSet<Rating>();
+        }
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Passwd { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        [ForeignKey("RoleId")]
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Rating> Ratings { get; set; }
+    }
+}
