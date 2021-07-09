348844a6a5a1791eb32faa3f2de1e95f077be038
SekouDosso <sekou.dosso@revature.net>
2021-07-09 15:33:39 -0400

    login test

--- P2UnitTests/UserModelTest.cs
+++ P2UnitTests/UserModelTest.cs
@@ -72,5 +72,58 @@ namespace P2UnitTests
 
 			}
 		}
+
+		
+	
+		[Fact]
+				public async Task LoginExistingUserCorrectly()
+				{
+					// arrange
+					//createa a player to inset into the inmemory db.
+					User u = new User()
+					{
+						Username = "Max",
+						Passwd = "Max",
+						FirstName = "Max",
+						LastName = "Max",
+						RoleId = 0,
+					};
+					bool result = false;
+					bool result1 = false;
+					bool result2 = false;
+
+					using (var context = new P2Context(options))
+					{
+						context.Database.EnsureDeleted();// delete any Db fro a previous test
+						context.Database.EnsureCreated();// create anew the Db... you will need ot seed it again.
+						context.Users.Add(u);
+						context.SaveChanges();
+					}
+					// act
+					// add to the In-Memory Db
+					//instantiate the inmemory db
+					using (var context = new P2Context(options))
+					{
+						//verify that the db was deleted and created anew
+						context.Database.EnsureDeleted();// delete any Db fro a previous test
+						context.Database.EnsureCreated();// create anew the Db... you will need ot seed it again.
+
+						//instantiate the Class that we are going to unit test
+						UserTest loginUserTest = new UserTest(context);
+						result = await loginUserTest.RegisterUserAsync(u);
+						context.SaveChanges();
+						result1 = await loginUserTest.UserLoginAsync(u);
+						result2 = await loginUserTest.LoginAsync(u);
+
+						//assert
+						// verify the the result was as expected
+						Assert.True(result);
+						Assert.True(result1);
+						Assert.True(result2);
+					}
+				}
 	}
+		
+
+	
 }
