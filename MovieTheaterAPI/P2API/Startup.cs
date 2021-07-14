3608671cf20d3d5fd86a2de7cc66d07920c97d7e
ssilva2021 <stephen.silva@raveture.net>
2021-07-14 14:07:26 -0400

    re-fixed CORS for local host and added bootstrap to index.html

--- MovieTheaterAPI/P2API/Startup.cs
+++ MovieTheaterAPI/P2API/Startup.cs
@@ -18,63 +18,73 @@ using BusinessLayer;
 
 namespace P2API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-      
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-
-      services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "testApp", Version = "v1" });
-      });
-      services.AddDbContext<P2Context>(options =>
-      {
-        if (!options.IsConfigured)
+        public Startup(IConfiguration configuration)
         {
-          options.UseSqlServer(Configuration.GetConnectionString("P2Database"));
+            Configuration = configuration;
+
         }
-      });
-      services.AddDistributedMemoryCache();
-      services.AddScoped<IUserService, UserService>();
-      services.AddScoped<IScheduleService, ScheduleService>();
-      services.AddScoped<ICommentService, CommentService>();
-      services.AddScoped<IMovieService, MovieService>();
-      services.AddScoped<IRatingService, RatingService>();
-      services.AddScoped<IRoleService, RoleService>();
-      services.AddScoped<ITheaterMovieService, TheaterMovieService>();
-      services.AddScoped<ITheaterService, TheaterService>();
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors((options) =>
+            {
+                options.AddPolicy(name: "dev", builder =>
+               {
+                   builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+               });
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "testApp", Version = "v1" });
+            });
+            services.AddDbContext<P2Context>(options =>
+            {
+                if (!options.IsConfigured)
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("P2Database"));
+                }
+            });
+            services.AddDistributedMemoryCache();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IScheduleService, ScheduleService>();
+            services.AddScoped<ICommentService, CommentService>();
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<IRatingService, RatingService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<ITheaterMovieService, TheaterMovieService>();
+            services.AddScoped<ITheaterService, TheaterService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "testApp v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("dev");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "testApp v1"));
-      }
-
-      app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
 }
