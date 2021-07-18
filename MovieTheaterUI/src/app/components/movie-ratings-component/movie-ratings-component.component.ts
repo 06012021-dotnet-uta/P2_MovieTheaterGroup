f8ba76ccaf0dc1c34f995fb256aeeb6a63dc0973
ssilva2021 <stephen.silva@raveture.net>
2021-07-18 18:53:47 -0400

    crude but working components for Comments and ratings

--- MovieTheaterUI/src/app/components/movie-ratings-component/movie-ratings-component.component.ts
+++ MovieTheaterUI/src/app/components/movie-ratings-component/movie-ratings-component.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { MovieServiceService } from 'src/app/services/movie.service.service';
+// import { MovieComments } from 'src/app/interfaces/movieComments';
+import { MovieRatings } from 'src/app/classes/movieratings';
 
 @Component({
   selector: 'app-movie-ratings-component',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieRatingsComponentComponent implements OnInit {
 
-  constructor() { }
+  lstMovieRatings?: MovieRatings[];
+  id: string = this.route.snapshot.paramMap.get('id')!;
+
+
+  constructor(    
+    private route: ActivatedRoute,
+    private movieService: MovieServiceService,
+    private location: Location) { }
 
   ngOnInit(): void {
+    this.getMovieRatings();
+    console.log(this.lstMovieRatings);
+
+  }
+  getMovieRatings(): void {
+    console.log(this.id);
+    
+    this.movieService.getMovieRatings(this.id)
+    .subscribe(
+      // data=>
+      // {
+      //   console.log(data);
+      //   // this.lstMovieComments = data;
+      //   // this.lstMovieComments = data;
+      //   // console.log(this.movie);
+      // }
+      // lstMovieComments => console.log(lstMovieComments),
+      lstMovieRatings => this.lstMovieRatings = lstMovieRatings
+    );
   }
 
 }
