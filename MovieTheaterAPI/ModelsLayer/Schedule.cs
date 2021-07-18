13c5a6cb223521cac8b520f98a33c2b559bf52a7
BrianCorbett <brian.corbett@revature.net>
2021-07-18 14:17:03 -0700

    'rando'

--- MovieTheaterAPI/ModelsLayer/Schedule.cs
+++ MovieTheaterAPI/ModelsLayer/Schedule.cs
@@ -11,33 +11,33 @@ namespace ModelsLayer
     public class Schedule
 #pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
     {
-    [Key]
-    public int ScheduleId { get; set; }
+        [Key]
+        public int ScheduleId { get; set; }
 
-    [Required]
-    [ForeignKey("TheaterId")]
-    public int TheaterId { get; set; }
+        [Required]
+        [ForeignKey("TheaterId")]
+        public int TheaterId { get; set; }
 
-    [Required]
-    [ForeignKey("MovieId")]
-    public string MovieId { get; set; }
+        [Required]
+        [ForeignKey("MovieId")]
+        public string MovieId { get; set; }
 
-    public DateTime ShowingTime { get; set; }
+        public DateTime ShowingTime { get; set; }
 
-    public virtual Movie Movie { get; set; }
-    public virtual Theater Theater { get; set; }
+        public virtual Movie Movie { get; set; }
+        public virtual Theater Theater { get; set; }
 
-    public override bool Equals(object obj)
-    {
-      if (obj is Schedule)
-      {
-        var that = obj as Schedule;
-        return (this.ScheduleId == that.ScheduleId &&
-          this.MovieId == that.MovieId &&
-          this.TheaterId == that.TheaterId &&
-          this.ShowingTime == that.ShowingTime);
-      }
-      return false;
+        public override bool Equals(object obj)
+        {
+            if (obj is Schedule)
+            {
+                var that = obj as Schedule;
+                return (this.ScheduleId == that.ScheduleId &&
+                  this.MovieId == that.MovieId &&
+                  this.TheaterId == that.TheaterId &&
+                  this.ShowingTime == that.ShowingTime);
+            }
+            return false;
+        }
     }
-  }
 }
