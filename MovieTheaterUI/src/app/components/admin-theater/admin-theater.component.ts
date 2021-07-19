dbfcffb439c81538d393d9874ae2a1bc029ea939
GregoryAgnew <gregory.agnew@revature.net>
2021-07-19 01:10:19 -0500

    1ampush

--- MovieTheaterUI/src/app/components/admin-theater/admin-theater.component.ts
+++ MovieTheaterUI/src/app/components/admin-theater/admin-theater.component.ts
@@ -1,10 +1,35 @@
-import { Component} from '@angular/core';
-import { TheaterListComponent } from '../theater-list/theater-list.component';
+import { Component, OnInit} from '@angular/core';
+import { Theater } from 'src/app/interfaces/theater';
+import { TheaterService } from 'src/app/services/theater.service';
 
 @Component({
   selector: 'app-admin-theater',
   templateUrl: './admin-theater.component.html',
   styleUrls: ['./admin-theater.component.css']
 })
-export class AdminTheaterComponent extends TheaterListComponent{
+export class AdminTheaterComponent implements OnInit{
+
+    theaters?: Theater[];
+
+    theaterName = '';
+    theaterLocation = '';
+
+constructor(private theaterService: TheaterService) {
+}
+
+    onSubmit(): void {
+        const newTheater: Theater = {
+            theaterLoc: this.theaterName,
+            theaterName: this.theaterName,
+            theaterId: 0
+        }
+        this.theaterService.addTheater(newTheater).subscribe()
+    }
+
+    ngOnInit(): void {
+        this.theaterService.getTheaters().subscribe(
+            theaters => this.theaters = theaters
+
+        );
+    }
 }
