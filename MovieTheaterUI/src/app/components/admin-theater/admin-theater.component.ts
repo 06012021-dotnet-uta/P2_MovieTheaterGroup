41ad72ca0dc0fcfca6291859e45f7bec38d7c5da
GregoryAgnew <gregory.agnew@revature.net>
2021-07-19 06:47:16 -0500

    theater add admin

--- MovieTheaterUI/src/app/components/admin-theater/admin-theater.component.ts
+++ MovieTheaterUI/src/app/components/admin-theater/admin-theater.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit} from '@angular/core';
+import { interval } from 'rxjs';
 import { Theater } from 'src/app/interfaces/theater';
 import { TheaterService } from 'src/app/services/theater.service';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-admin-theater',
@@ -10,11 +13,11 @@ import { TheaterService } from 'src/app/services/theater.service';
 export class AdminTheaterComponent implements OnInit {
 
     theaters?: Theater[];
-
+    theater?: Theater;
     theaterName = '';
     theaterLocation = '';
 
-    constructor(private theaterService: TheaterService) {
+    constructor(private userService: UserService, private theaterService: TheaterService) {
     }
 
     onSubmit(): void {
@@ -26,10 +29,46 @@ export class AdminTheaterComponent implements OnInit {
         this.theaterService.addTheater(newTheater).subscribe()
     }
 
+    currentUser: User = {
+        userId: 1,
+        username: 'test',
+        passwd: '',
+        firstName: '',
+        lastName: '',
+        roleId: 0
+    };
+
+
     ngOnInit(): void {
         this.theaterService.getTheaters().subscribe(
             theaters => this.theaters = theaters
 
         );
+        interval(1000).subscribe(x => this.checkLogin())
+    }
+
+    //update(): void {
+    //    this.theater = 
+    //    this.theaterService.updateTheater(this.theater)
+    //}
+
+    admin = false;
+    login!: boolean;
+    checkLogin(): void {
+        this.currentUser = this.userService.GetCurrentUser();
+
+        if (this.currentUser.roleId === 1) {
+            this.admin = true;
+        }
+        else {
+            this.admin = false;
+        }
+
+        if (this.currentUser.roleId === 0) {
+            this.login = false;
+        }
+        else {
+            this.login = true;
+        }
     }
 }
